3d56c1fa5a2b7e90ca0fde4536eb5d463a6c1513
L. Corbalan <lcorbalan@gmail.com>
2023-03-17 00:10:05 -0300

    codigo2

--- Program.cs
+++ Program.cs
@@ -1,3 +1,3 @@
-﻿string marca = "Ford";
-int modelo = 2000;
-string st = string.Format("Es un {0} año {1}", marca, modelo);
+﻿double r = 2.417;
+Console.WriteLine($"Valor = {r:0.0}");
+Console.WriteLine($"Valor = {r:0.00}");
