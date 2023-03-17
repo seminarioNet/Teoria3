ca731ab19df02aa16cdde1daa3900e0abe01a48d
L. Corbalan <lcorbalan@gmail.com>
2023-03-17 00:17:47 -0300

    codigo5

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
 ﻿double[]? vector = new double[10];
+Procesar(null, 1, 1);
+Procesar(vector, 10, 1);
+Procesar(vector, -1, 1);
+Procesar(vector, 1, -10);
 Procesar(vector, 1, 1);
-
 
 void Procesar(double[]? v, int i, int c)
 {
